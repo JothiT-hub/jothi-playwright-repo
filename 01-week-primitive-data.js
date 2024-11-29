4c6e6b994e7bf7852e0f70d79b00a1e77b04f053
JothiT-hub <jothithulasi04@gmail.com>
2024-11-29 20:47:21 +0530

    Add files via upload

---
+++ 01-week-primitive-data.js
@@ -0,0 +1,16 @@
+/*Declare variables using `let` for different data types in JavaScript, and verify their types using `typeof`,
+including an uninitialized variable
+*/
+
+let firstName =`Jothi`;
+let companyName = `Softura`;
+let mobileNumber = 9816238912;
+let isAutomation = true;
+let hasPlaywright = "yes it has";
+
+console.log("The datatype of FirstName is" + typeof firstName);
+console.log("The datatype of CompanyName is" + typeof companyName);
+console.log("The datatype of MobileNumber is" + typeof mobileNumber);
+console.log("The datatype of isAutomation is" + typeof isAutomation);
+console.log("The datatype of hasplaywright is" + typeof hasPlaywright);
+
